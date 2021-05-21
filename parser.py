9659d2074a7ed12aefc9eb0d13fbfa5d102df7c8
Buntoman <kolyabyntov@gmail.com>
2021-05-21 13:36:23 +0300

    cod def "get_content"

--- parser.py
+++ parser.py
@@ -12,9 +12,27 @@ def get_html(url, params=None):
     return r
 
 
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('div', class_='company-item')
+    objects = []
+    for item in items:
+         objects.append({
+             'title': item.find('div', class_="company-item__title").get_text(strip=True),
+             'OGRN': item.find('div', class_="company-item-info").get_text(strip=True),
+             'OKPO': item.find('div', class_="company-item-info").get_text(strip=True),
+             'Status': item.find('div', class_="company-item-info").get_text(strip=True),
+             'Date': item.find('div', class_="company-item-info").get_text(strip=True),
+             'Capital': item.find('div', class_="company-item-info").get_text(strip=True)
+         })
+    print(item)
+    print(len(objects))
 def parse():
     html = get_html(URL)
-    print(html)
+    if html.status_code == 200:
+        get_content(html.text)
+    else:
+        print("Error")
 
 
 parse()
