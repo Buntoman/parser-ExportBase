dbf173fc62e43b46c8d3082eefdcad984533e6fc
Buntoman <kolyabyntov@gmail.com>
2021-05-22 22:10:35 +0300

    fix get_content and add sql

--- parser.py
+++ parser.py
@@ -1,3 +1,6 @@
+import pymysql
+from config import host, user, password, db_name
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -16,16 +19,28 @@ def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.find_all('div', class_='company-item')
     objects = []
+    len(items)
     for item in items:
-         objects.append({
-             'title': item.find('div', class_="company-item__title").get_text(strip=True),
-             'OGRN': item.find('div', class_="company-item-info").get_text(strip=True),
-             'OKPO': item.find('div', class_="company-item-info").get_text(strip=True),
-             'Status': item.find('div', class_="company-item-info").get_text(strip=True),
-             'Date': item.find('div', class_="company-item-info").get_text(strip=True),
-             'Capital': item.find('div', class_="company-item-info").get_text(strip=True)
+        #print (item)
+        Status = item.find('span', class_="attention-text") or item.find('span', class_="warning-text")
+        if Status:
+             Status = Status.get_text()
+        else:
+             Status = 'Действующая'
+        def null_value(value):
+            if item.find(text=value):
+                return item.find(text=value).find_next('dd').get_text()
+            else:
+                return 'null'
+        objects.append({
+            'title': item.find('div', class_="company-item__title").get_text(strip=True),
+            'OGRN': null_value('ОГРН'),
+            'INN': null_value('ИНН'),
+            'Status': Status,
+            'Date': null_value('Дата регистрации'),
+            'Capital': null_value('Уставный капитал')
          })
-    print(item)
+    print(objects)
     print(len(objects))
 def parse():
     html = get_html(URL)
@@ -36,3 +51,17 @@ def parse():
 
 
 parse()
+
+try:
+    connection = pymysql.connect(
+        host=host,
+        port=3306,
+        user=user,
+        password=password,
+        database=db_name,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    print('successfully connected...')
+except Exception as ex:
+    print('conection refused...')
+    print(ex)
